5a7906a49d3db1fe91c039aff7088126a171eae5
Surya T N <suryatej.nim@yahoo.com>
2022-09-11 16:18:02 +0000

    Added bare-bones UI layout for CommonItem functional component

--- src/components/CommentItem/index.js
+++ src/components/CommentItem/index.js
@@ -1,1 +1,14 @@
-// Write your code here
+import './index.css'
+
+const CommentItem = props => {
+  const {itemData, likeActionHandler, deleteActionHandler} = props
+
+  return (
+    <div className="comment-item-container">
+      <div className="comment-item-content-container"></div>
+      <div className="comment-item-actions-container"></div>
+    </div>
+  )
+}
+
+export default CommentItem
