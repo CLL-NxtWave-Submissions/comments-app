7fb1c92fb9db8851c5282f31902f0205b130ed35
Surya T N <suryatej.nim@yahoo.com>
2022-09-11 13:42:25 +0000

    Implemented Comments class component to render intended UI (without the comments list) and form submit event handler

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,3 +1,7 @@
+import {Component} from 'react'
+import {v4 as uuidv4} from 'uuid'
+import './index.css'
+
 const initialContainerBackgroundClassNames = [
   'amber',
   'blue',
@@ -8,4 +12,85 @@ const initialContainerBackgroundClassNames = [
   'light-blue',
 ]
 
-// Write your code here
+export default class Comments extends Component {
+  state = {
+    name: '',
+    comment: '',
+    commentsList: [],
+  }
+
+  onAddComment = addCommentEvent => {
+    addCommentEvent.preventDefault()
+
+    this.setState(prevCommentsState => {
+      const {name, comment, commentsList} = prevCommentsState
+
+      const newComment = {
+        commentId: uuidv4(),
+        authorName: name,
+        postedComment: comment,
+        creationDateTime: new Date(),
+        isLiked: false,
+      }
+
+      const updatedCommentsList = [...commentsList, newComment]
+      const updatedCommentsState = {
+        name: '',
+        comment: '',
+        commentsList: updatedCommentsList,
+      }
+
+      return updatedCommentsState
+    })
+  }
+
+  render() {
+    const {name, comment, commentsList} = this.state
+
+    return (
+      <div className="comments-app-bg-container">
+        <div className="comments-content-container">
+          <h1 className="comments-header">Comments</h1>
+          <div className="top-img-and-form-container">
+            <img
+              className="comments-img"
+              src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png "
+              alt="comments"
+            />
+            <form
+              className="form-content-container"
+              onSubmit={this.onAddComment}
+            >
+              <p className="form-header">
+                Say something about 4.0 Technologies
+              </p>
+              <input
+                type="text"
+                className="form-input"
+                placeholder="Your Name"
+                value={name}
+                onChange={this.onNameChange}
+              />
+              <textarea
+                className="form-textarea"
+                placeholder="Your Comment"
+                value={comment}
+                onChange={this.onCommentChange}
+              />
+              <button type="submit" className="form-submit-button">
+                Add Comment
+              </button>
+            </form>
+          </div>
+          <hr />
+          <div className="comments-container">
+            <div className="comments-count-container">
+              <p className="comments-count">{commentsList.length}</p>
+              <p className="comments-header">Comments</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
