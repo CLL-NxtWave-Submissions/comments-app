15bc82881dfc3fa1eec4887853d69a9b10371f23
Surya T N <suryatej.nim@yahoo.com>
2022-09-11 17:35:17 +0000

    Implemented the intended UI layout in CommentItem component and added styling for comment like button

--- src/components/CommentItem/index.js
+++ src/components/CommentItem/index.js
@@ -1,3 +1,4 @@
+import {formatDistanceToNow} from 'date-fns'
 import './index.css'
 
 const CommentItem = props => {
@@ -12,11 +13,78 @@ const CommentItem = props => {
     backgroundColorClassName,
   } = itemData
 
+  const timeSinceCommentCreationAsText = formatDistanceToNow(creationDateTime)
+  const classNameStringForAuthorNameInitialElement = `comment-item-author-initial ${backgroundColorClassName}`
+  const classNameStringForLikeTextElement = isLiked
+    ? 'comment-item-liked-text'
+    : 'comment-item-like-text'
+
+  const authorNameInitial = authorName.slice(0, 1).toUpperCase()
+
+  const onClickCommentLikeButton = () => likeActionHandler(commentId)
+  const onClickCommentDeleteButton = () => deleteActionHandler(commentId)
+
   return (
-    <div className="comment-item-container">
-      <div className="comment-item-content-container"></div>
-      <div className="comment-item-actions-container"></div>
-    </div>
+    <li className="comment-item-container">
+      <div className="comment-item-content-container">
+        <div className="comment-item-author-initial-container">
+          <p className={classNameStringForAuthorNameInitialElement}>
+            {authorNameInitial}
+          </p>
+        </div>
+
+        <div className="comment-item-details-container">
+          <div className="comment-item-author-and-creation-date-time-container">
+            <p className="comment-item-author-name">{authorName}</p>
+            <p className="comment-item-time-to-now">
+              {timeSinceCommentCreationAsText}
+            </p>
+          </div>
+
+          <p className="comment-item-comment-text">{postedComment}</p>
+        </div>
+      </div>
+
+      <div className="comment-item-actions-container">
+        <div className="comment-item-like-action-container">
+          <button
+            type="button"
+            className="comment-item-like-action-button"
+            onClick={onClickCommentLikeButton}
+          >
+            {isLiked ? (
+              <img
+                className="comment-item-like-img"
+                src="https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png"
+                alt="like"
+              />
+            ) : (
+              <img
+                className="comment-item-like-img"
+                src="https://assets.ccbp.in/frontend/react-js/comments-app/liked-img.png"
+                alt="like"
+              />
+            )}
+          </button>
+
+          <p className={classNameStringForLikeTextElement}>Like</p>
+        </div>
+
+        <button
+          type="button"
+          className="comment-item-delete-action-button"
+          onClick={onClickCommentDeleteButton}
+        >
+          <img
+            className="comment-item-delete-icon-img"
+            src="https://assets.ccbp.in/frontend/react-js/comments-app/delete-img.png"
+            alt="delete"
+          />
+        </button>
+      </div>
+
+      <hr className="horizontal-line-separator" />
+    </li>
   )
 }
 
